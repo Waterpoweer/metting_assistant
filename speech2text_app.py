26f078f701dafdd95c7720b2a98141ba7b67f89b
Waterpoweer <redaouardi6@gmail.com>
2026-04-27 22:05:20 +0100

    update

--- speech2text_app.py
+++ speech2text_app.py
@@ -1,12 +1,50 @@
 from simple_speech2text import ASR
+from simple_llm import llm
 import gradio as gr
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+import torch
+import os
+
+#######------------- Prompt Template-------------####
+
+# This template is structured based on LLAMA2. If you are using other LLMs, feel free to remove the tags
+temp = """
+<s><<SYS>>
+List the key points with details from the context: 
+[INST] The context : {context} [/INST] 
+<</SYS>>
+"""
+# here is the simplified version of the prompt template
+# temp = """
+# List the key points with details from the context: 
+# The context : {context} 
+# """
+
+pt = PromptTemplate(
+    input_variables=["context"],
+    template= temp)
+
+prompt_to_LLAMA2 = LLMChain(llm=llm, prompt=pt)
+
+def fgd(speech) :
+
+    transcrib = ASR(speech)
+
+
+
+    result = prompt_to_LLAMA2.run(transcrib)
+
+    return result
+
+
 
 
 audio_input = gr.Audio(sources="upload", type="filepath")  # Audio input
 output_text = gr.Textbox()  # Text output
 
 # Create the Gradio interface with the function, inputs, and outputs
-iface = gr.Interface(fn=ASR, 
+iface = gr.Interface(fn=fgd, 
                      inputs=audio_input, outputs=output_text, 
                      title="Audio Transcription App",
                      description="Upload the audio file")
