1cfcca61ac47be0edad9e05771934f8358fe62fd
Waterpoweer <redaouardi6@gmail.com>
2026-04-27 21:55:34 +0100

    initial commit

---
+++ speech2text_app.py
@@ -0,0 +1,15 @@
+from simple_speech2text import ASR
+import gradio as gr
+
+
+audio_input = gr.Audio(sources="upload", type="filepath")  # Audio input
+output_text = gr.Textbox()  # Text output
+
+# Create the Gradio interface with the function, inputs, and outputs
+iface = gr.Interface(fn=ASR, 
+                     inputs=audio_input, outputs=output_text, 
+                     title="Audio Transcription App",
+                     description="Upload the audio file")
+
+# Launch the Gradio app
+iface.launch(debug = True  ,  share=True)
