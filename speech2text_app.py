6bf8c260d2a4362d3220e7da8dd4dddedd74fae3
Waterpoweer <redaouardi6@gmail.com>
2026-04-28 20:00:34 +0100

    Update speech2text_app.py
    recorded audio adding

--- speech2text_app.py
+++ speech2text_app.py
@@ -41,13 +41,13 @@ def fgd(speech) :
 
 
 
-audio_input = gr.Audio(sources="upload", type="filepath")  # Audio input
+audio_input = gr.Audio(sources=["upload","microphone"] ,  type="filepath")  # Audio input
 output_text = gr.Textbox()  # Text output
 
 # Create the Gradio interface with the function, inputs, and outputs
 iface = gr.Interface(fn=fgd, 
                      inputs=audio_input, outputs=output_text, 
-                     title="Audio Transcription App",
+                     title="REDA Transcription App",
                      description="Upload the audio file")
 
 # Launch the Gradio app
